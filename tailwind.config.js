a8cfbf0422be832ecdd0e5e53b16e380658e23da
fx-got <faceit.top1.gg@gmail.com>
2025-03-24 21:25:29 +0100

    aktualizacja plików na laptopie4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "15px", // było 12px
+        base: "18px", // było 16px
+        lg: "21px", // było 18px
+        xl: "24px", // było 20px
+        "2xl": "27px", // było 24px
+        "3xl": "30px", // było 28px
+        "4xl": "36px", // było 32px
+        "5xl": "48px", // było 40px
+      },
+    },
   },
   plugins: [],
 };
