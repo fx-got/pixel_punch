95f121eceb709b9026ff0471de1fa3fe58dba45e
fx-got <faceit.top1.gg@gmail.com>
2025-03-24 20:52:22 +0100

    aktualizacja plików na laptopie

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // <--- to jest kluczowe
+  images: {
+    unoptimized: true, // jeśli używasz <Image />, to to pomaga przy static export
+  },
 };
 
 export default nextConfig;
